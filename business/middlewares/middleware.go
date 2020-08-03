c5bb53d2bcfbd3999286fdde51d13aef60893591
11s14033 <agildian509@gmail.com>
2020-08-03 14:57:36 +0700

    Define middleware

---
+++ business/middlewares/middleware.go
@@ -0,0 +1,12 @@
+package middlewares
+
+import (
+	"net/http"
+)
+
+func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
+	return func(res http.ResponseWriter, req *http.Request) {
+		res.Header().Set("Content-Type", "application/json")
+		next(res, req)
+	}
+}
