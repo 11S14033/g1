3863bec8bcab2bf16f0345beed898c09e3cad521
11s14033 <agildian509@gmail.com>
2020-08-07 19:31:31 +0700

    Define [room usecase service]

---
+++ services/room/usecase/room_usecase.go
@@ -0,0 +1,59 @@
+package usecase
+
+import (
+	"context"
+	"log"
+
+	"github.com/11s14033/g1/services/room/commons/models"
+	"github.com/11s14033/g1/services/room/repository"
+)
+
+type roomUseCase struct {
+	roomRepository repository.RoomRepository
+}
+
+func NewRoomUseCase(repo repository.RoomRepository) RoomUseCase {
+	return &roomUseCase{
+		roomRepository: repo,
+	}
+}
+
+func (roomUseCase *roomUseCase) GetRoomByID(rid uint64) (room models.Room, err error) {
+
+	room, err = roomUseCase.roomRepository.GetRoomByID(rid)
+
+	if err != nil {
+		log.Fatalln("Error [roomUseCase][GetRoomByID], when calling [roomRepository][GetRoomByID], cause", err)
+	}
+	return room, nil
+}
+func (roomUseCase *roomUseCase) SaveRoom(ctx context.Context, room models.Room) (err error) {
+
+	err = roomUseCase.roomRepository.SaveRoom(ctx, room)
+	if err != nil {
+		log.Fatalln("Error [roomUseCase][SaveRoom], when calling [roomRepository][SaveRoom], cause", err)
+	}
+	return nil
+}
+func (roomUseCase *roomUseCase) UpdateRoom(ctx context.Context, room models.Room) (newRoom models.Room, err error) {
+
+	newRoom, err = roomUseCase.roomRepository.UpdateRoom(ctx, room)
+	if err != nil {
+		log.Fatalln("Error [roomUseCase][UpdateRoom], when calling [roomRepository][UpdateRoom], cause", err)
+	}
+	return newRoom, nil
+}
+func (roomUseCase *roomUseCase) DeleteRoom(ctx context.Context, rid uint64) (err error) {
+	err = roomUseCase.roomRepository.DeleteRoom(ctx, rid)
+	if err != nil {
+		log.Fatalln("Error [roomUseCase][DeleteRoom], when calling [roomRepository][DeleteRoom], cause", err)
+	}
+	return nil
+}
+func (roomUseCase *roomUseCase) GetRooms(ctx context.Context) (rooms []models.Room, err error) {
+	rooms, err = roomUseCase.roomRepository.GetRooms(ctx)
+	if err != nil {
+		log.Fatalln("Error [roomUseCase][GetRooms], when calling [roomRepository][GetRooms], cause", err)
+	}
+	return rooms, nil
+}
