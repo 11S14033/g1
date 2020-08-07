326631264a722a74c3d143c3ab841ee8e5ab6fd0
11s14033 <agildian509@gmail.com>
2020-08-07 19:24:15 +0700

    Define [room repository]

---
+++ services/room/repository/repository.go
@@ -0,0 +1,15 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/11s14033/g1/services/room/commons/models"
+)
+
+type RoomRepository interface {
+	GetRoomByID(rid uint64) (room models.Room, err error)
+	SaveRoom(ctx context.Context, room models.Room) (err error)
+	UpdateRoom(ctx context.Context, room models.Room) (newRoom models.Room, err error)
+	DeleteRoom(ctx context.Context, rid uint64) (err error)
+	GetRooms(ctx context.Context) (rooms []models.Room, err error)
+}
