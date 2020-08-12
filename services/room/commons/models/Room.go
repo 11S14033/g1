0857f2b503887e17f76d9643d3b5ed8c6f948ec2
11s14033 <agildian509@gmail.com>
2020-08-12 10:06:02 +0700

    [upadate][typo room model]

--- services/room/commons/models/Room.go
+++ services/room/commons/models/Room.go
@@ -4,10 +4,9 @@ import (
 	"time"
 )
 
-//`gorm:"size:255;not null;unique" json:"room_name"`
 type Room struct {
 	ID          uint64    `gorm:"primary_key;auto_increment" json:"id"`
-	RoomName    string    `gorm:"size:255;not null" json:"room_name"`
+	RoomName    string    `gorm:"size:255;not null;unique" json:"room_name"`
 	Owner       string    `gorm:"size:255;not null" json:"owner"`
 	Address     string    `gorm:"not null" json:"address"`
 	City        string    `gorm:"not null" json:"city"`
@@ -15,5 +14,5 @@ type Room struct {
 	Description string    `gorm:"not null" json:"description"`
 	Type        uint32    `gorm:"not null" json:"type"`
 	CreatedAt   time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
-	UpdateAt    time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"update_at"`
+	UpdatedAt   time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
