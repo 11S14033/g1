ee6488a331982e476fabbf856365a2b161bda2ae
11s14033 <agildian509@gmail.com>
2020-08-12 10:00:18 +0700

    [upadte][room model]

--- services/room/commons/models/Room.go
+++ services/room/commons/models/Room.go
@@ -4,9 +4,10 @@ import (
 	"time"
 )
 
+//`gorm:"size:255;not null;unique" json:"room_name"`
 type Room struct {
 	ID          uint64    `gorm:"primary_key;auto_increment" json:"id"`
-	RoomName    string    `gorm:"size:255;not null;unique" json:"room_name"`
+	RoomName    string    `gorm:"size:255;not null" json:"room_name"`
 	Owner       string    `gorm:"size:255;not null" json:"owner"`
 	Address     string    `gorm:"not null" json:"address"`
 	City        string    `gorm:"not null" json:"city"`
