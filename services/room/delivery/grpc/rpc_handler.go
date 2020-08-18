b6e583836961d27cf383a20467c59e0bbe202acf
11s14033 <agildian509@gmail.com>
2020-08-18 10:45:41 +0700

    [update][error handling isu]

--- services/room/delivery/grpc/rpc_handler.go
+++ services/room/delivery/grpc/rpc_handler.go
@@ -2,23 +2,29 @@ package grpc
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
+	"google.golang.org/grpc/codes"
+
 	"github.com/11s14033/g1/services/room/commons/models"
 	roompb "github.com/11s14033/g1/services/room/commons/pb"
+	utils "github.com/11s14033/g1/services/room/delivery/utils"
 )
 
 func dataToPBRoom(data models.Room) *roompb.Room {
 	return &roompb.Room{
 
-		Id:           data.ID,
-		RoomName:     data.RoomName,
-		Addres:       data.Address,
-		City:         data.City,
-		Province:     data.Province,
-		Descriptions: data.Description,
-		Owner:        data.Owner,
+		Id:          data.ID,
+		RoomName:    data.RoomName,
+		Addres:      data.Address,
+		City:        data.City,
+		Province:    data.Province,
+		Description: data.Description,
+		Owner:       data.Owner,
+		CreatedAt:   data.CreatedAt.String(),
+		UpdatedAt:   data.UpdatedAt.String(),
 	}
 }
 
@@ -26,80 +32,83 @@ func (rs RoomRPCService) GetRooms(req *roompb.GetRoomsRequest, stream roompb.Roo
 	datas, err := rs.usecase.GetRooms(context.Background())
 
 	if err != nil {
-		log.Printf("[Error][roomGRPCService][GetRooms]-[when calling][roomUseCase][GetRooms]-[%v]", err)
+		desc := fmt.Sprintf("[Error][roomGRPCService][GetRooms]-[when calling][roomUseCase][GetRooms]-[%v]", err)
+		log.Println(desc)
+		return utils.ErrorRPC(codes.Internal, desc)
 	}
 	for _, data := range datas {
-		stream.Send(&roompb.GetRoomsResponse{
-			Room: dataToPBRoom(data),
-		})
+		stream.Send(dataToPBRoom(data))
 	}
 	return nil
 }
-func (rs RoomRPCService) GetRoomByID(ctx context.Context, req *roompb.GetRoomByIDRequest) (*roompb.GetRoomByIDResponse, error) {
+func (rs RoomRPCService) GetRoomByID(ctx context.Context, req *roompb.GetRoomByIDRequest) (*roompb.Room, error) {
 	var err error
 	rid := req.GetRoomId()
 
 	room, err := rs.usecase.GetRoomByID(ctx, rid)
 	if err != nil {
-		log.Printf("[Error][roomGRPCService][GetRoomByID]-[when calling][roomUseCase][GetRoomByID]-[%v]", err)
+		desc := fmt.Sprintf("[Error][roomGRPCService][GetRoomByID]-[when calling][roomUseCase][GetRoomByID]-[%v]", err)
+		log.Println(desc)
+		return nil, utils.ErrorRPC(codes.Internal, desc)
 	}
-	return &roompb.GetRoomByIDResponse{
-		Room: dataToPBRoom(room),
-	}, nil
+	return dataToPBRoom(room), nil
 
 }
-func (rs RoomRPCService) UpdateRoomByID(ctx context.Context, req *roompb.UpdateRoomByIDRequest) (*roompb.UpdateRoomByIDResponse, error) {
-	room := req.GetRoom()
+func (rs RoomRPCService) UpdateRoomByID(ctx context.Context, req *roompb.Room) (*roompb.Room, error) {
+
 	newRoom := models.Room{
-		ID:          room.GetId(),
-		RoomName:    room.GetRoomName(),
-		Address:     room.GetAddres(),
-		City:        room.GetCity(),
-		Description: room.GetDescriptions(),
-		Owner:       room.GetOwner(),
-		Province:    room.GetProvince(),
-		Type:        room.GetType(),
+		ID:          req.GetId(),
+		RoomName:    req.GetRoomName(),
+		Address:     req.GetAddres(),
+		City:        req.GetCity(),
+		Description: req.GetDescription(),
+		Owner:       req.GetOwner(),
+		Province:    req.GetProvince(),
+		Type:        req.GetType(),
+		UpdatedAt:   time.Now(),
 	}
 	data, err := rs.usecase.UpdateRoom(ctx, newRoom)
 	if err != nil {
-		log.Printf("[Error][roomGRPCService][UpdateRoomByID]-[when calling][roomUseCase][UpdateRoomByID]-[%v]", err)
+		desc := fmt.Sprintf("[Error][roomGRPCService][UpdateRoomByID]-[when calling][roomUseCase][UpdateRoomByID]-[%v]", err)
+		log.Println(desc)
+		return nil, utils.ErrorRPC(codes.Internal, desc)
 	}
-	return &roompb.UpdateRoomByIDResponse{
-		Room: dataToPBRoom(data),
-	}, nil
+	return dataToPBRoom(data), nil
 }
 
-func (rs RoomRPCService) SaveRoom(ctx context.Context, req *roompb.CreateRoomRequest) (*roompb.CreateRoomResponse, error) {
-	room := req.GetRoom()
+func (rs RoomRPCService) SaveRoom(ctx context.Context, req *roompb.Room) (*roompb.Room, error) {
 	data := models.Room{
-		ID:          room.GetId(),
-		Address:     room.GetAddres(),
-		City:        room.GetCity(),
-		Description: room.GetDescriptions(),
-		Owner:       room.GetOwner(),
-		Province:    room.GetProvince(),
-		RoomName:    room.GetRoomName(),
-		Type:        room.GetType(),
+		ID:          req.GetId(),
+		Address:     req.GetAddres(),
+		City:        req.GetCity(),
+		Description: req.GetDescription(),
+		Owner:       req.GetOwner(),
+		Province:    req.GetProvince(),
+		RoomName:    req.GetRoomName(),
+		Type:        req.GetType(),
 		CreatedAt:   time.Now(),
-		UpdateAt:    time.Now(),
+		UpdatedAt:   time.Now(),
 	}
 
 	err := rs.usecase.SaveRoom(ctx, data)
 	if err != nil {
-		log.Printf("[Error][roomGRPCService][SaveRoom]-[when calling][roomUseCase][SaveRoom]-[%v]", err)
+		desc := fmt.Sprintf("[Error][roomGRPCService][SaveRoom]-[when calling][roomUseCase][SaveRoom]-[%v]", err)
+		log.Println(desc)
+		return nil, utils.ErrorRPC(codes.Internal, desc)
 	}
 
-	return &roompb.CreateRoomResponse{
-		Room: dataToPBRoom(data),
-	}, nil
+	return dataToPBRoom(data), nil
 }
 
-func (rs RoomRPCService) DeleteRoom(ctx context.Context, req *roompb.DeleteRoomRequest) (res *roompb.DeleteRoomResponse, err error) {
+func (rs RoomRPCService) DeleteRoom(ctx context.Context, req *roompb.DeleteRoomRequest) (*roompb.DeleteRoomResponse, error) {
 	rid := req.GetRoomId()
-	err = rs.usecase.DeleteRoom(context.Background(), rid)
+	err := rs.usecase.DeleteRoom(context.Background(), rid)
 
 	if err != nil {
-		log.Printf("[Error][roomGRPCService][DeleteRoom]-[when calling][roomUseCase][DeleteRoom]-[%v]", err)
+
+		desc := fmt.Sprintf("[Error][roomGRPCService][DeleteRoom]-[when calling][roomUseCase][DeleteRoom]-[%v]", err)
+		log.Println(desc)
+		return nil, utils.ErrorRPC(codes.Internal, desc)
 	}
 	return &roompb.DeleteRoomResponse{
 		RoomId: rid,
